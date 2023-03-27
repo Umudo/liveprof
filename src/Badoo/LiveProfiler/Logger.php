9d286cc26240bd01f49aae5d4daee92df65deac0
Umutcan Güney <Umudo@users.noreply.github.com>
2023-03-27 14:07:53 +0300

    Update Logger.php

--- src/Badoo/LiveProfiler/Logger.php
+++ src/Badoo/LiveProfiler/Logger.php
@@ -34,7 +34,7 @@ class Logger implements LoggerInterface
      * @param string $message
      * @param array $context
      */
-    public function log($level, $message, array $context = array())
+    public function log($level, Stringable|string $message, array $context = array()): void
     {
         $log_string = $this->getLogMsg($level, $message, $context);
         file_put_contents($this->logfile, $log_string, FILE_APPEND);
